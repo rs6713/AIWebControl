fc80e4698a0e16f663edca76bbd49df541311e34
Becks <becks.simpson@outlook.com>
2018-02-09 18:17:34 +0000

    Nearly completed frontend of demo

--- public/application.js
+++ public/application.js
@@ -1,5 +1,22 @@
 var mainApplicationModuleName= 'trainMe';
-var mainApp= angular.module(mainApplicationModuleName, []);
+var mainApp= angular.module(mainApplicationModuleName, ['ngMaterial', 'ngMessages']);
 
 mainApp.controller('mainController',['$scope', '$timeout','$mdToast' , "$mdDialog",function($scope, $timeout,$mdToast, $mdDialog){
+    //578 433
+    $scope.newTrigger="";
+    $scope.newImages=[];
+    var captureVideo;
+    var getVideo=function(){
+        canvas = document.getElementById('canvas');
+        context = canvas.getContext('2d');
+        context.drawImage(video,0,0, 320, 180); 
+        $scope.newImages.push(canvas.toDataURL("image/png"));
+    }
+    $scope.getImages=function(){
+        console.log("I'm getting the video");
+        captureVideo=setInterval(getVideo, 500);
+    }
+    $scope.stopImages=function(){
+        clearInterval(captureVideo);
+    }
 }]);
--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -10,10 +10,10 @@ var heights=[];
 
 for(var i=0; i< $items.length; i++){
     var $item= $($items[i]);
-    var height= $item.offset().top + documentHeight/2;
-    if(height>wrapperHeight){
-        height=wrapperHeight;
-    }
+    var height= $item.offset().top + documentHeight/2+ $item.height()/2;
+    //if(height>wrapperHeight){
+    //   height=wrapperHeight;
+    //}
     positions.push(height);
 }
 
@@ -42,7 +42,49 @@ $("#line .shimmer").first().addClass("nodeActive");
 var $nodes= $(".node");
 var $shimmers=$(".shimmer");
 
+$( ".iterim" ).each(function( index ) {
+    var previous=$( this ).prev().offset().top+$(this).prev().height();
+    var next=$( this ).next().offset().top;
+    var topoffset=(next-previous)/2 +"px";
+    var leftoffset=Math.floor((10+ 60*Math.random())* window.innerWidth/100) ;
+    console.log(topoffset, leftoffset);
+    $(this).offset({"top":topoffset, "left":leftoffset});
+});
+
+var errorCallback = function(e) {
+    console.log('Rejected', e);
+}
+
+var video;
+var canvas;
+var context;
+
+// Converts canvas to an image
+function convertCanvasToImage(canvas) {
+	var image = new Image();
+    image.src = canvas.toDataURL("image/png");
+	return image;
+}
+    // Not showing vendor prefixes.
+    navigator.getUserMedia({video: true, audio: true}, function(localMediaStream) {
+        video = document.querySelector('video');
+        video.src = window.URL.createObjectURL(localMediaStream);
+    
+        // Note: onloadedmetadata doesn't fire in Chrome when using it with getUserMedia.
+        // See crbug.com/110938.
+        video.onloadedmetadata = function(e) {
+        // Ready to go. Do some stuff.
+        };
+    }, errorCallback);
+
+
+
 $(document).ready(function(){
+    //canvas = document.getElementById('canvas');
+    //context = canvas.getContext('2d');
+    //context.drawImage(video,0,0, 160, 120);
+    //var image = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
+
     $( "#start" ).click(function() {
         console.log("Start clicked");
         $("#contentContainer").css({"visibility":"visible"});
@@ -52,11 +94,15 @@ $(document).ready(function(){
         $("#contentContainer").addClass("fadeIn");
         $("#line").addClass("fadeIn");
     });
+
+    $("#demo").click(function(){
+        $("#demoContainer").css({"visibility":"visible"});
+    });
 });
 
 //Can also add customisation so alternates the animation style based on odd even
 $("#contentContainer").scroll(function(){
-    console.log("Scrolling", $("#contentContainer").scrollTop());
+    //console.log("Scrolling", $("#contentContainer").scrollTop());
     var current=0;
     var nodeTop= $("#contentContainer").scrollTop()+ documentHeight+1;
     for(var i=0; i<positions.length;i++){
@@ -79,6 +125,6 @@ $("#contentContainer").scroll(function(){
 $nodes.each( function( index ) {
     var $node = $( this );
     $node.click( function() {
-      $("#contentContainer").animate({ scrollTop: (positions[ index ] - documentHeight/1.5)}, 1000 );
+      $("#contentContainer").animate({ scrollTop: (positions[ index ]- documentHeight-1)}, 1000 );
     });
 })
